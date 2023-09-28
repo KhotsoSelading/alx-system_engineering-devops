eeb8c0416a32a5ec0c153cdb204f7c2d68799ab1
KhotsoSelading <k.selading007@gmail.com>
2023-09-28 08:02:51 +0200

    pp file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,34 +1,78 @@
-# Install Nginx package
+# add stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# install nginx
 package { 'nginx':
-  ensure => 'installed',
+  ensure     => 'installed',
 }
 
-# Ensure Nginx service is enabled and running
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+# allow HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'Ã®i.*nginx\' > /dev/null 2>&1',
 }
 
-# Configure Nginx server block
-file { '/etc/nginx/sites-available/default':
-  ensure => file,
-  content => template('nginx/default.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# Create an HTML file with "Hello World!"
+# create index file
 file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
+  content => "Hello World!\n",
 }
 
-# Create a 301 redirect for /redirect_me
-nginx::resource::location { 'redirect_me':
-  location => '/redirect_me',
-  ensure   => 'present',
-  alias    => '/var/www/html',
-  rewrite  => '^/redirect_me$ / permanent;',
-  require  => File['/var/www/html/index.html'],
+# create index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# add redirection and error page
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location  /404.html {
+        internal;
+    }
+}",
+}
+
+# restart nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# start service nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
