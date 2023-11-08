ef9cc68edbd631bad1a48d85c4bac7fefe2a06ba
KhotsoSelading <k.selading007@gmail.com>
2023-11-08 20:18:01 +0200

    0x17

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,6 @@
 # Fixes wordpress config file typo
-exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
